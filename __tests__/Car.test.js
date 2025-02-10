5d7213f2f0800716691043145ab9890573f5395a
na-reum <devnareum@gmail.com>
2025-02-10 14:46:34 +0900

    feat: 테스트 수정

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -6,18 +6,20 @@ describe("Car 클래스 기능 테스트", () => {
   beforeEach(() => {
     car = new Car("na-reum");
   });
+  describe("Car 객체 생성 테스트", () => {
+    it("Car객체는 생성시 string으로 넘긴 text를 name으로 가진다.", () => {
+      expect(car.name).toBe("na-reum");
+    });
 
-  it("자동차는 이름을 가진다", () => {
-    expect(car.name).toBe("na-reum");
+    it("Car객체의 초기 position 값은 0이다.", () => {
+      expect(car.position).toBe(0);
+    });
   });
-
-  it("자동차는 위치 값을 가지며, 초기값은 0이다", () => {
-    expect(car.position).toBe(0);
-  });
-
-  it("자동차는 전진할 수 있으며 한 번에 1만큼 전진한다", () => {
-    const initialPosition = car.position;
-    car.moveForward();
-    expect(car.position).toBe(initialPosition + 1);
+  describe("moveForward", () => {
+    it("moveForward 실행시 position값에 1을 더한다.", () => {
+      const initialPosition = car.position;
+      car.moveForward();
+      expect(car.position).toBe(initialPosition + 1);
+    });
   });
 });
